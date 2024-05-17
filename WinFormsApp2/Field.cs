46ab0e40c458d2ffe4ba835c5f23cbc0f9cbd057
miki-oic <miki@oic.jp>
2024-05-17 12:25:40 +0900

    プロジェクト ファイルを追加します。

---
+++ WinFormsApp2/Field.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp2
+{
+    public class Field
+    {
+        public string getName()
+        {
+
+            return "";
+
+        }
+    }
+
+    public class Castle : Field
+    {
+
+    }
+
+    public class Player : Magician
+    {
+        public string getName()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface Magician
+    {
+
+        string getName();
+
+    }
+}
