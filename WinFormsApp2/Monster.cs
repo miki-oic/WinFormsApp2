03a078f9cebd0eeedbd1dc55db8803cfd7c0ced9
riku- <riku-@LAPTOP-16KS218U>
2024-06-21 11:14:55 +0900

    モンスターの追加

---
+++ WinFormsApp2/Monster.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp2
+{
+    public class Monster
+    {
+       private int HP;
+       private int AC;
+       private int EXP;
+       private char Race;
+
+        
+    }
+}
