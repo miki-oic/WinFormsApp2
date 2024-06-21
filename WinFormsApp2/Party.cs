b7c8e1be041335a1f3d9d57ab817ae470320fafd
C2270 <C2270@oic.jp>
2024-06-21 11:11:54 +0900

    partyの追加ぁぁぁ

---
+++ WinFormsApp2/Party.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp2
+{
+    public class Party
+    {
+        int id;
+
+    }
+}
