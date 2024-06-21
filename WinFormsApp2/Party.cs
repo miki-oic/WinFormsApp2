7795d688bc8b17e1e533b049031c05534f64b19e
C2270 <C2270@oic.jp>
2024-06-21 11:38:53 +0900

    修正の修正

--- WinFormsApp2/Party.cs
+++ WinFormsApp2/Party.cs
@@ -8,7 +8,7 @@ namespace WinFormsApp2
 {
     public class Party
     {
-        private int Id;
-        private List<Player> Member = new List<Player>();
+        private int id;
+        private List<Player> member = new List<Player>();
     }
 }
