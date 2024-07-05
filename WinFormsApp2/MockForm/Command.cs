4faa933953af9053c7d5291dde80496f42221928
Link <link080514@gmail.com>
2024-07-05 12:24:40 +0900

    ログの仮システムを作成

---
+++ WinFormsApp2/MockForm/Command.cs
@@ -0,0 +1,59 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp2.MockForm
+{
+    public class Command
+    {
+        string command = "行動なし";
+        public virtual string GetCommand()
+        {
+            return command;
+        }
+    }
+
+    public class Attack : Command
+    {
+        public override string GetCommand()
+        {
+            return "攻撃をした！";
+        }
+    }
+
+    public class Gurid : Command
+    {
+        public override string GetCommand()
+        {
+            return "防御した!";
+        }
+    }
+
+    public class Magic : Command
+    {
+        public override string GetCommand()
+        {
+            return "魔法を唱えた";
+        }
+    }
+
+    public class Escape : Command
+    {
+        public override string GetCommand()
+        {
+            return "逃走を計った！";
+        }
+    }
+
+
+    public class CurseUnLock : Command 
+    {
+        public override string GetCommand()
+        {
+            return "呪いを解いた！";
+        }
+    }
+}
--- WinFormsApp2/MockForm/MockForm_k.cs
+++ WinFormsApp2/MockForm/MockForm_k.cs
@@ -12,22 +12,36 @@ namespace WinFormsApp2.MockForm
 {
     public partial class MockForm_k : Form
     {
-        int count = 0; 
+        int count = 0;
+
+        bool changePlayer = false;
+        int changeCount = 0;
+
+        private MockPlayer player;
+
+        MockPlayer.CharaTag charaTag = MockPlayer.CharaTag.Player1;
+
+        private Command command;
 
         public MockForm_k()
         {
             InitializeComponent();
+            
+            player = new HeroModel();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            AddLogEntry("よっぴー", "攻撃をした");
+            command = new Attack();
+            AddLogEntry(player.GetName(), command.GetCommand());
+            ChangePlayer();
         }
 
         public void AddLogEntry(string name, string command)
         {
             string log = name + "は" + command;
             listBox1.Items.Add(log);
+
         }
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
@@ -37,7 +51,9 @@ namespace WinFormsApp2.MockForm
 
         private void button2_Click(object sender, EventArgs e)
         {
-            AddLogEntry("メンヘラ", "防御を行った");
+            command = new Gurid();
+            AddLogEntry(player.GetName(), command.GetCommand());
+            ChangePlayer();
         }
 
         private void MockForm_k_Load(object sender, EventArgs e)
@@ -45,8 +61,47 @@ namespace WinFormsApp2.MockForm
             count++;
             label1.Text = count.ToString();
         }
-    }
 
+        private void button3_Click(object sender, EventArgs e)
+        {
+            command = new Magic();
+            AddLogEntry(player.GetName(), command.GetCommand());
+            ChangePlayer();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            command = new Escape();
+            AddLogEntry(player.GetName(), command.GetCommand());
+            ChangePlayer();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            command = new CurseUnLock();
+            AddLogEntry(player.GetName(), command.GetCommand());
+            ChangePlayer();
+        }
+
+        private void ChangePlayer()
+        {
+            changeCount++;
+            switch (changeCount)
+            {
+                case 0:
+                    player = new HeroModel();
+                    break;
+                case 1:
+                    player = new SamuraiModel();
+                    break;
+                case 2:
+                    player = new ThiefModel();
+                    changeCount = -1;
+                    break;
+            }
+        }
+    }
+    
     public interface MockPlayer
     {
         enum CharaTag
@@ -62,18 +117,66 @@ namespace WinFormsApp2.MockForm
         /// <returns></returns>
         int GetHP();
 
+        /// <summary>
+        /// 名前を返す
+        /// </summary>
+        /// <returns></returns>
         string GetName();
 
+
     }
 
-    public interface MockHero : MockPlayer
+    public abstract class MockPlayerModel : MockPlayer
     {
-        int GetSkillDamage();
+        int hp = 100;
+        string name = "名無し";
+
+        public MockPlayerModel(string _name)
+        {
+            name = _name;
+        }
+
+        public int GetHP() { return hp; }
+
+        public string GetName() { return name; }
     }
 
-    public abstract class MockPlayerModel
+    public interface Hero : MockPlayer
     {
 
     }
 
+    public class HeroModel : MockPlayerModel, Hero
+    {
+        public HeroModel() : base("勇者")
+        {
+
+        }
+    }
+
+    public interface Samurai : MockPlayer
+    {
+
+    }
+
+    public class SamuraiModel : MockPlayerModel, Samurai
+    {
+        public SamuraiModel() : base("侍")
+        {
+
+        }
+    }
+
+    public interface Thief : MockPlayer
+    {
+
+    }
+
+    public class ThiefModel : MockPlayerModel, Thief
+    {
+        public ThiefModel() : base("盗賊")
+        {
+
+        }
+    }
 }
