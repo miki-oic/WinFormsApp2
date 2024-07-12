21e350c535e31b7a4a4e38b4a46714b35f74144d
諏訪野ヒロ <62730538+suwanohiro@users.noreply.github.com>
2024-07-12 10:24:32 +0900

    コンストラクタを追加

--- WinFormsApp2/TCPClient/TCPClient.cs
+++ WinFormsApp2/TCPClient/TCPClient.cs
@@ -8,5 +8,13 @@ namespace WinFormsApp2.TCPClient
 {
     class TCPClient
     {
+        string _ip;
+        ushort _port;
+
+        public TCPClient(string ip, ushort port)
+        {
+            _ip = ip;
+            _port = port;
+        }
     }
 }
