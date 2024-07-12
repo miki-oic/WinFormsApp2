f48c7f32ed38c70b0daa122bab67b0f05af89ba6
C2399@oic.jp <C2399@oic.jp>
2024-07-12 11:15:27 +0900

    敵Mock追加

---
+++ WinFormsApp2/Enemy.cs
@@ -0,0 +1,95 @@
+﻿namespace WinFormsApp2
+{
+    public interface Enemy : Attacker
+    {
+        /// <summary>
+        /// 耐久力を返します。
+        /// </summary>
+        /// <returns>耐久力</returns>
+        int GetHitPoint();
+
+        /// <summary>
+        /// 指定された攻撃者からのダメージを受けます。
+        /// </summary>
+        /// <param name="attacker">攻撃者</param>
+        /// <returns>このオブジェクト</ret urns>
+        Enemy DamagedBy(Attacker attacker);
+
+        /// <summary>
+        /// 大丈夫かどうかを返す。
+        /// </summary>
+        /// <returns>大丈夫な場合 true</returns>
+        bool IsOk();
+
+        /// <summary>
+        /// 気絶しているかどうかを返す。
+        /// </summary>
+        /// <returns>気絶している場合 true</returns>
+        bool IsCollapsed();
+
+        /// <summary>
+        /// オブザーバーを追加します。
+        /// </summary>
+        /// <param name="observer"></param>
+        /// <returns>オブザーバー</returns>
+        Enemy AddObserver(Observer observer);
+    }
+
+    public class NullEnemy : Enemy
+    {
+
+        private static Enemy enemy = new NullEnemy();
+
+        private NullEnemy()
+        {
+
+        }
+
+        public static Enemy GetInstance()
+        {
+
+            return enemy;
+
+        }
+
+        public Enemy AddObserver(Observer observer)
+        {
+
+            return this;
+
+        }
+
+        public int Attack()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Enemy DamagedBy(Attacker attacker)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetHitPoint()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsCollapsed()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsOk()
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+
+        //public class Enemy
+        //{
+
+        //}
+    }
+}
